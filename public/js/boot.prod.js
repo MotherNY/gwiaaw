6ab6d5dd310ecf6eb6f07b925e4b92bab0ff136c
joemccann <joseph.isaac@gmail.com>
2013-05-23 13:18:48 -0500

    version bump

--- public/js/boot.prod.js
+++ public/js/boot.prod.js
@@ -2,7 +2,7 @@
 var $lib = ('__proto__' in {} ? 'zepto.min' : 'jquery.min')
 require(["log",  $lib, "fastclick" ], function(l, zepto){
   log($lib + ', Fastclick and Log loaded...')
-  require(["guesswho-0.0.1-4.min"], function(guesswho){
+  require(["guesswho-0.0.1-5.min"], function(guesswho){
 	  new FastClick(document.body)
 		log('FastClick enabled on document.')
 		log('All JS files loaded...')
